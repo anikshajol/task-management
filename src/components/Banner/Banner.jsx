6e1d4375f03a0109fd27282d586f5ebda52622e4
anikshajol <anikshajol@gmail.com>
2023-12-21 18:14:42 +0600

    banner added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,23 +1,28 @@
+import useAuth from "../../hooks/useAuth";
+
 const Banner = () => {
+  const { user } = useAuth();
   return (
-    <div>
+    <div className="">
       <div
-        className="hero min-h-screen"
+        className="hero min-h-[75vh] mb-8 mt-4"
         style={{
           backgroundImage:
             "url(https://images.unsplash.com/photo-1611224923853-80b023f02d71?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGFzayUyMG1hbmFnZW1lbnR8ZW58MHx8MHx8fDA%3D)",
         }}
       >
-        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-overlay bg-black bg-opacity-90"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+            <h2 className="text-2xl md:text-3xl lg:text-7xl font-bold ">
+              Welcome;
+            </h2>
+            <p className="mt-6 text-2xl">
+              Stay Organized and Productive
+              <br />{" "}
+              <span className="text-[#800080]">Explore our To-Do Website!</span>
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            {!user && <button className="btn btn-primary">Get Started</button>}
           </div>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,11 @@
+import Banner from "../components/Banner/Banner";
 import Container from "../components/ui/Container";
-import useAuth from "../hooks/useAuth";
 
 const Home = () => {
-  const { user } = useAuth();
   return (
     <>
       <Container>
-        <div
-          className="flex my-32 flex-col md:flex-row items-center 
-         md:items-start"
-        >
-          <div className="">
-            <h2 className="text-7xl font-bold w-[80%]">
-              Organize Your Work
-              <br /> <span className="text-[#800080]">& Life, finally</span>
-            </h2>
-            <p className="mt-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-              commodi sit corrupti sint quo et dicta accusamus reprehenderit
-              omnis
-            </p>
-            <div className="mt-6">
-              {!user && (
-                <button className="btn bg-[#800080] text-white">
-                  Book a Service
-                </button>
-              )}
-              <button className="btn btn-ghost">Read More</button>
-            </div>
-            <div className="divider"></div>
-          </div>
-
-          <div className="h-[400px] bg-red-500 rounded-lg overflow-hidden">
-            <img
-              className="w-full h-full object-cover"
-              src="https://images.unsplash.com/photo-1611224923853-80b023f02d71?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGFzayUyMG1hbmFnZW1lbnR8ZW58MHx8MHx8fDA%3D"
-              alt=""
-            />
-          </div>
-        </div>
+        <Banner></Banner>
       </Container>
     </>
   );
